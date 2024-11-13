e2d8a1d85a815a9d3ea581ffb4c6fdbe52084c3e
Kevincabj7 <kevinwiner27@gmail.com>
2024-11-13 15:11:52 -0300

    Add files via upload

---
+++ trabajo 1 kevin.cpp
@@ -0,0 +1,11 @@
+//codigo de alumno:kevin carlos muñoz
+//link del git personal:https://github.com/Kevincabj7/Kevin-Mu-oz.git
+//link del git grupal (lider:miguel angel chura crespo): https://github.com/Kevincabj7
+#incluir <stdio.>
+float cm_a_pulgadas(float centimetros) {
+    return centimetros / 2.54;
+}
+float pulgadas_a_cm(float pulgadas) {
+    return pulgadas * 2.54;
+    return 0;
+}
